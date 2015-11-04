56204576ae6dc8933c1e00f4daa6c0567eff3312
Avi Samloff <avi.samloff@gmail.com>
2015-11-03 16:59:33 -0800

    update

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,9 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(details => {
+chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,7 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Option');
+
+//Disable on all pages
+  //if disabled
+    //change badge text to 'OFF'
