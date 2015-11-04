167644dd218db7fb6dda09e5e4aa99fab475560f
Avi Samloff <avi.samloff@gmail.com>
2015-11-03 20:33:20 -0800

    include angular

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,7 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Content script');
+
+var pageContents = document.body.innerText; //string containing all the text from the current tab
+
+console.log(pageContents)
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,15 @@
 'use strict';
-
-console.log('\'Allo \'Allo! Popup');
+var angular = require('angular');
+//enables links within the popup to be opened into a new tab
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tabs.create({active: true, url: location});
+            };
+        })();
+    }
+});
